6e5d7b44fe55ff878e909186620b3706ffac26e7
YInglai Jo <joyinglai@qq.com>
2016-11-22 11:21:53 +0100

    dashboard table group function in progress, connect server data done

--- client/app/admin-interface/adminDashboard/adminDashboard.component.js
+++ client/app/admin-interface/adminDashboard/adminDashboard.component.js
@@ -9,9 +9,26 @@ export class AdminDashboardComponent {
 
 
       /*@ngInject*/
-    constructor() {
+    constructor($http) {
         this.message = 'Hello';
+        this.$http = $http;
+        this.$http.get('/api/datas/')
+            .then(response => {
+            console.log(response.data);
+        this.datas = response.data;
+        })
+
+        this.checkdatas = [{
+            personid: '123123123',
+            requester: 'abc',
+            timestamp: '123',
+            allowed: 'yes',
+            data: 'data'
+        }]
     }
+
+
+    $onInit(){}
 
 
 }
