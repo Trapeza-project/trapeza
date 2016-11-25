330284f2b441b0027f40884f618252a9ed2da4d4
duff92 <joakimlarsson92@gmail.com>
2016-11-25 16:25:25 +0100

    Added basic layout for pending requests, waiting for Marcus to change endpoint

--- client/app/user-interface/account/pendingRequests/pendingRequests.controller.js
+++ client/app/user-interface/account/pendingRequests/pendingRequests.controller.js
@@ -2,8 +2,27 @@
 
 export default class UserPendingRequestsController {
 
+  personid = '111111111111';
+  toggleCompanyInfo = false;
   /*@ngInject*/
-  constructor() {
+  constructor($http) {
+    this.$http = $http;
+    this.getPendingRequests();
+  }
 
+  getPendingRequests() {
+    this.$http.get('/api/requests/personpending/' + this.personid)
+      .then(response => {
+        console.log(response.data);
+      });
+  }
+
+  toggleInfo() {
+    if(this.toggleCompanyInfo) {
+      this.toggleCompanyInfo = false;
+    }
+    else {
+      this.toggleCompanyInfo = true;
+    }
   }
 }
