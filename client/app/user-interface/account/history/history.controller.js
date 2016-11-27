3975049ad6e726849c21022f90f92d57fe953ec6
duff92 <joakimlarsson92@gmail.com>
2016-11-27 15:40:27 +0100

    Added history search page

--- client/app/user-interface/account/history/history.controller.js
+++ client/app/user-interface/account/history/history.controller.js
@@ -2,8 +2,19 @@
 
 export default class UserHistoryController {
 
+  historyRequests = [];
+  personid = '197001011234';
   /*@ngInject*/
-  constructor() {
+  constructor($http) {
+    this.$http = $http;
+    this.getAllRequests();
+  }
 
+  getAllRequests() {
+    this.$http.get('/api/requests/person/' + this.personid)
+      .then(response => {
+        console.log('history',response.data.history);
+        this.historyRequests = response.data.history;
+      });
   }
 }
