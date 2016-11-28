8b02e8f002a159045d00c12405dfea984ca7218f
duff92 <joakimlarsson92@gmail.com>
2016-11-28 14:51:31 +0100

    WIP, permission settings

--- client/app/user-interface/account/permissionSettings/permissonSettings.controller.js
+++ client/app/user-interface/account/permissionSettings/permissonSettings.controller.js
@@ -1,10 +1,18 @@
 'use strict';
 
+import awesomeplete from 'awesomplete';
+
 export default class UserPermissionSettingsController {
 
-
+  trustedList = [];
+  actors = [];
+  infoTypes = [];
   /*@ngInject*/
-  constructor() {
+  constructor($http) {
+
+    this.$http = $http;
+    this.getInfoTypes();
+    this.getActors();
 
     $('#allow-all-toggle' ).click(function() {
       $('.allow-all input').each(function() {
@@ -12,7 +20,7 @@ export default class UserPermissionSettingsController {
       });
     });
 
-    $('#allow-trusted-toggle' ).click(function() {
+    $('#allow-trusted-toggle').click(function() {
       $('.allow-trusted input').each(function() {
         $(this)[0].checked = true;
       });
@@ -23,5 +31,31 @@ export default class UserPermissionSettingsController {
         $(this)[0].checked = true;
       });
     });
+  }
+
+  getInfoTypes() {
+    this.$http.get('/api/infotypes/usertypes')
+      .then(response => {
+        console.log(response.data);
+        this.infoTypes = response.data.datatypes;
+      });
+  }
+
+  getActors() {
+    var that = this;
+    this.$http.get('/api/actors/')
+      .then(response =>
+      {
+
+        response.data.forEach(function(item) {
+          that.actors.push(item.name);
+        });
+        awesomeplete.list = that.actors;
+        console.log(awesomeplete.list);
+      });
+  }
+
+  addToTrusted() {
+    console.log($("#search-actor").text());
   }
 }
